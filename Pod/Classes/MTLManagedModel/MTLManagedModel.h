421cb56a0836b21e3b176c682384a9f16f27dbf6
vlad gorbenko <mojidabckuu.22.06.92@gmail.com>
2015-08-16 16:07:46 +0400

    Add mamanged object model.

---
+++ Pod/Classes/MTLManagedModel/MTLManagedModel.h
@@ -0,0 +1,15 @@
+//
+//  MTLManagedModel.h
+//  Pods
+//
+//  Created by vlad gorbenko on 8/16/15.
+//
+//
+
+#import <CoreData/CoreData.h>
+
+#import <Mantle/MTLModel.h>
+
+@interface MTLManagedModel : NSManagedObject <MTLModel>
+
+@end
