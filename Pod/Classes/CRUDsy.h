c597d004848e95ee75443394baea26bde7870879
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-27 17:03:47 +0400

    Add CRUDsy header file.

--- Pod/Classes/CRUDsy.h
+++ Pod/Classes/CRUDsy.h
@@ -9,5 +9,11 @@
 #ifndef CRUDsy_h
 #define CRUDsy_h
 
+#import "NSObject+API.h"
+
+#import "NSValueTransformer+ModelId.h"
+
+#import "APIRouter.h"
+#import "APIRouteModelCriteria.h"
 
 #endif /* CRUDsy_h */
