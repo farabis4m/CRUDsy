6c1eff1ac8c6ce4038875be8b6e3038da2442a5a
Vlad Gorbenko <mojidabckuu.22.06.92@gmail.com>
2016-03-21 11:51:36 +0400

    add missing files.

---
+++ Pod/Classes/APIRouter/Constants/APIRouteKeys.h
@@ -0,0 +1,30 @@
+//
+//  APIRouteKeys.h
+//  Pods
+//
+//  Created by vlad gorbenko on 10/27/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const APIIndexKey;
+extern NSString *const APICreateKey;
+extern NSString *const APIShowKey;
+extern NSString *const APIUpdateKey;
+extern NSString *const APIDeleteKey;
+extern NSString *const APIPatchKey;
+
+extern NSString *const APIActionKey;
+
+extern NSString *const APIRequestKey;
+extern NSString *const APIResponseKey;
+
+extern NSString *const APITypeKey;
+
+extern NSString *const APIMethodKey;
+extern NSString *const APIRouteKey;
+extern NSString *const APIURLKey;
+extern NSString *const APIFormatKey;
+
+extern NSString *const APIPrefixKey;
---
+++ Pod/Classes/APIRouter/Constants/APIRouteKeys.m
@@ -0,0 +1,30 @@
+//
+//  APIRouteKeys.m
+//  Pods
+//
+//  Created by vlad gorbenko on 10/27/15.
+//
+//
+
+#import "APIRouteKeys.h"
+
+NSString *const APIIndexKey = @"index";
+NSString *const APICreateKey = @"create";
+NSString *const APIShowKey = @"show";
+NSString *const APIUpdateKey = @"update";
+NSString *const APIPatchKey = @"patch";
+NSString *const APIDeleteKey = @"delete";
+
+NSString *const APIActionKey = @"action";
+
+NSString *const APIRequestKey = @"request";
+NSString *const APIResponseKey = @"response";
+
+NSString *const APITypeKey = @"type";
+
+NSString *const APIMethodKey = @"method";
+NSString *const APIRouteKey = @"route";
+NSString *const APIURLKey = @"url";
+NSString *const APIFormatKey = @"format";
+
+NSString *const APIPrefixKey = @"prefix";
